5a0027aeb98a49f3809073cf9874d616bfb75509
subraja <subraja04@gmail.com>
2017-07-14 15:42:06 -0400

    modified

--- MyFirstGitProject/src/FirstEGit.java
+++ MyFirstGitProject/src/FirstEGit.java
@@ -5,7 +5,7 @@ public class FirstEGit {
 	public static void main(String[] args) {
 		
 		System.out.println("My first GitProject1");
-		System.out.println("test 1");
+		System.out.println("test 1 modified");
 		
 		
 		
